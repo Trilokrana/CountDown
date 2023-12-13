0cba3d6560120c81086239cc0281abe127ae72b4
Trilok_rana <ranatrilok1234@gmail.com>
2023-12-13 23:38:45 +0530

    CountDown

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [hour, setHour] = useState(0)
+  const [minutes, setMinutes] = useState(0)
+  const [seconds, setSeconds] = useState(0)
+  const [timer, setTimer] = useState(false)
+
+  useEffect(() => {
+    let Interval
+    if (timer) {
+      Interval = setInterval(() => {
+        if (minutes === 0 && seconds === 0) {
+          setTimer(false);
+          clearInterval(Interval);
+        } else {
+          if (seconds === 0) {
+            setMinutes((prevMinutes) => prevMinutes - 1);
+            setSeconds(59);
+          } else {
+            setSeconds((prevSeconds) => prevSeconds - 1);
+          }
+        }
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(Interval);
+    };
+  }, [timer, minutes, seconds]);
+
+
+  const MinutesChange = (e) => {
+    const value = parseInt(e.target.value, 10);
+    setMinutes(isNaN(value) ? 0 : value);
+    if (e.target.value === '') {
+      setSeconds(0);
+    }
+  };
+
+  const StartTimer = () => {
+    if (!timer) {
+      setTimer(true);
+    }
+  };
+
+  const ResetTimer = () => {
+    setTimer(false);
+    setMinutes(0);
+    setSeconds(0);
+  };
+
+  const PauseTimer = () => {
+    setTimer(false);
+  };
+
+  const formatTime = (time) => {
+    return time.toString().padStart(2, '0');
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='App'>
+        <p>Enter Minutes</p>
+        <input type='text' name='name' onChange={MinutesChange} />
+        <h1>Timer: {formatTime(hour)} : {formatTime(minutes)} : {formatTime(seconds)}</h1>
+        <button onClick={StartTimer}>Start</button>
+        <button onClick={PauseTimer}>Stop</button>
+        <button onClick={ResetTimer}>Reset</button>
+      </div>
+    </>
+  )
 }
+export default App
 
-export default App;
+
+
+
+
+
+
+
+
+
+
